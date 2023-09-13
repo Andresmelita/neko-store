cf749e7d82522eda82f0230278b4c61552aa7b66
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-09-12 22:38:21 -0300

    feat: components added

---
+++ src/views/admin/ProductsView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="pt-[200px]">
+        <h1>Desde Admin</h1>
+    </div>
+</template>
---
+++ src/views/admin/SalesView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="p-t-[200px]">
+        <h1>Resumen de ventas</h1>
+    </div>
+</template>
+
