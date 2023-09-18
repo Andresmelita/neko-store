684927dd03f50cc18da7c593e90a3dcf954fe560
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-09-18 14:48:06 -0300

    feat: upload image to firebase storage

--- src/composables/useImage.js
+++ src/composables/useImage.js
@@ -1,6 +1,29 @@
+import { useFirebaseStorage } from 'vuefire'
+import { ref as storageRef, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { uid } from 'uid'
+
 export default function useImage() {
+    const storage = useFirebaseStorage() 
+
     const onFileChange= e => {
-        console.log(e.target.files[0])
+        const file = e.target.files[0]
+        const filename = uid() + '.jpg'
+        const sRef = storageRef(storage, '/productos/' + filename)
+
+        //subir el archivo
+        const uploadTask = uploadBytesResumable(sRef, file)
+        uploadTask.on('state_changed',
+            () => {},
+            (error) => console.log(error),
+            () => {
+                // Upload is complete
+                getDownloadURL(uploadTask.snapshot.ref)
+                    .then((downloadURL)=>{
+                        console.log(downloadURL)
+                    })
+            }
+
+        )
     }
     return {
         onFileChange
