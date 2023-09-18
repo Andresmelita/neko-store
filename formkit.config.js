d4d9607b1a235aa5425e8eb5bb9509ffe22233d6
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-09-17 23:41:35 -0300

    feat: css in inputs

--- formkit.config.js
+++ formkit.config.js
@@ -5,13 +5,16 @@ const config = {
         classes: generateClasses({
             global:{
                 label: 'block mb-1 font-bold text-lg',
-                message:'text-red-500',
-                wrapper:'space-y-2 mb-3'
+                message: 'text-red-500',
+                wrapper: 'space-y-2 mb-3',
+                input: 'w-full p-3 border border-gray-300 rounded text-gray-700 placeholder-gray-400'
             },
             text: {
-                message:'$reset text-red-400'
+                message: '$reset text-red-400'
             },
             file: {
+                noFiles: 'block my-2',
+                fileItem: 'hidden'
             },
             select: {
             },
