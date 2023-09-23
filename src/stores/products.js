43b907eb721a9be975d3ad7d89b5b72c79d2b778
Carlos Andrés Salazar Melita <carlos.salazar.me@usach.cl>
2023-09-23 00:39:34 -0300

    feat: deleting records

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,32 +1,35 @@
-import { defineStore } from 'pinia'
-import { computed } from 'vue'
-import { useFirestore, useCollection } from 'vuefire'
-import { collection, addDoc, where, query, limit, orderBy, updateDoc } from 'firebase/firestore'
+import { computed, ref } from 'vue'
+import { defineStore } from 'pinia'
+import { useFirestore, useCollection, useFirebaseStorage } from 'vuefire'
+import { collection, addDoc, where, query, limit, orderBy, updateDoc, doc, getDoc, deleteDoc } from 'firebase/firestore'
+import { ref as storageRef, deleteObject } from 'firebase/storage'
 
 export const useProductsStore = defineStore('products', () => {
 
     const db = useFirestore()
+    const storage = useFirebaseStorage()
+
+    const selectedCategory = ref(1)
 
     const categories = [
-        { id: 1, name: 'Sudaderas' },
-        { id: 2, name: 'Tenis' },
-        { id: 3, name: 'Lentes' },
+        { id: 1, name: 'Sudaderas'},
+        { id: 2, name: 'Tenis'},
+        { id: 3, name: 'Lentes'},
     ]
-
-    const q = query(
+    
+    const q = query( 
         collection(db, 'products'),
-        //where('category', '==', 3)
-        //limit(10)
-        orderBy('name', 'asc')
+        orderBy('availability', 'asc')
     )
-    const productsCollection = useCollection(q)
+
+    const productsCollection = useCollection( q )
 
     async function createProduct(product) {
-        await addDoc(collection(db, 'products'), product)
+        await addDoc( collection( db, 'products'), product)
     }
 
     async function updateProduct(docRef, product) {
-        const {image, url, ...values} = product
+        const { image, url, ...values} = product
         if(image.length) {
             await updateDoc(docRef, {
                 ...values,
@@ -37,9 +40,23 @@ export const useProductsStore = defineStore('products', () => {
         }
     }
 
-    const categoryOptions = computed(()=>{
+    async function deleteProduct(id) {
+        if(confirm('¿Eliminar Producto?')) {
+            const docRef = doc(db, 'products', id)
+            const docSnap = await getDoc(docRef)
+            const {image} = docSnap.data()
+            const imageRef = storageRef(storage, image)
+
+            await Promise.all([
+                deleteDoc(docRef),
+                deleteObject(imageRef)
+            ])
+        }
+    }
+
+    const categoryOptions = computed(() => {
         const options = [
-            {label: 'Seleccione', value: '', attrs: {disabled: true}},
+            {label: 'Seleccione', value: '', attrs: {disabled: true } },
             ...categories.map(category => (
                 {label: category.name, value: category.id}
             ))
@@ -47,14 +64,23 @@ export const useProductsStore = defineStore('products', () => {
         return options
     })
 
-    const noResults = computed(()=> productsCollection.value.length === 0)
+    const noResults = computed(() => productsCollection.value.length === 0)
+
+    const filteredProducts = computed(() => {
+        return productsCollection.value
+            .filter( product => product.category === selectedCategory.value)
+            .filter( product => product.availability > 0)
+    })
 
     return {
         createProduct,
         updateProduct,
+        deleteProduct,
         productsCollection,
+        categories,
+        selectedCategory,
         categoryOptions,
-        noResults
+        noResults,
+        filteredProducts
     }
-})
-
+});
